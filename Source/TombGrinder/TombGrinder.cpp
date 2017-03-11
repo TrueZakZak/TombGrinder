fad361bf81f68739cfa5315f94f65d262b631077
DESKTOP-1NIMDMQ\eugeneshb <evgeniyshb@gmail.com>
2017-03-11 16:03:29 +0300

    ue4 project

---
+++ Source/TombGrinder/TombGrinder.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TombGrinder.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TombGrinder, "TombGrinder" );
---
+++ Source/TombGrinder/TombGrinderGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TombGrinderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOMBGRINDER_API ATombGrinderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
